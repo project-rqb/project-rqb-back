aa2837b34d06c2c38b1cbcc8186ffeb47c9a6fca
UNNNYOU <unnnyou@gmail.com>
2024-07-09 23:34:51 +0900

    [add] githubとの連携
    githubとomniauthを連携する設定を作成しました。
    また、これに伴ってgemの追加とsessions_controllerを作成しています。

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'api/v1/bases#index'
   namespace :api do
     namespace :v1 do
+      get 'auth/:provider/callback', to: 'sessions#create'
     end
   end
 end

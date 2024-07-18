8794f286f3571c3d7c51ed76ae857829fdafb9a1
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2024-07-18 16:53:00 +0900

    [clean] Lintチェック

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,5 +1,4 @@
 class QuestionSerializer < ActiveModel::Serializer
   attributes :uuid, :title, :body, :status
-  
   belongs_to :user, serializer: UserSerializer
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :name
-  
   has_many :questions, serializer: QuestionSerializer
 end

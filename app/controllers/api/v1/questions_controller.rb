1ab55a1f419663a00e154c842ac706a90ae55e30
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2024-07-26 13:38:26 +0900

    [clean] Questionsコントローラー 親クラスの修正

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::QuestionsController < ApplicationController
+class Api::V1::QuestionsController < Api::V1::BasesController
   def index
     questions = Question.includes(:user).all
     render json: questions, each_serializer: QuestionSerializer

5b2b75bd281d5b3150e8b9469273ae39484a21b3
のぞみ_44 <nozomi.muscle.10.02@gmail.com>
2024-07-27 20:39:20 +0900

    [add] QuestionsController create・count_all_questionsアクションの追加

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -3,4 +3,26 @@ class Api::V1::QuestionsController < Api::V1::BasesController
     questions = Question.includes(:user).all
     render json: questions, each_serializer: QuestionSerializer
   end
+
+  def create
+    question = Question.new(question_params)
+    question.user = @current_user
+
+    if question.save
+      render json: question, status: :created, serializer: QuestionSerializer
+    else
+      render json: { errors: question.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def count_all_questions
+    all_count = Question.count
+    render json: { count: all_count }
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:uuid, :title, :body, :status)
+  end
 end

6abbb0928c43d3eaedece7aebfebd62ce185795e
topi0247 <karubona.ra247@gmail.com>
2024-08-25 15:57:43 +0900

    fix:ユーザー作成と質問作成のseedを修正

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,17 +1,39 @@
-3.times do |i|
-  user = User.create!(
+# 3.times do |i|
+#   user = User.create!(
+#     uid: i + 1,
+#     provider: "github",
+#     github_uid: "github_#{i + 1}",
+#     name: "testname_#{i + 1}",
+#     term_id: rand(1..10),
+#     profile: "This profile is test_#{i + 1}."
+#   )
+
+#   user.questions.create!(
+#     uuid: SecureRandom.uuid,
+#     title: "This title is test_#{i + 1}.",
+#     body: "This body is test_#{i + 1}.",
+#     status: 0
+#   )
+# end
+
+10.times do |i|
+  user = User.find_or_create_by(
     uid: i + 1,
-    provider: "github",
+    provider: 'github',
     github_uid: "github_#{i + 1}",
     name: "testname_#{i + 1}",
     term_id: rand(1..10),
     profile: "This profile is test_#{i + 1}."
   )
 
-  user.questions.create!(
-    uuid: SecureRandom.uuid,
-    title: "This title is test_#{i + 1}.",
-    body: "This body is test_#{i + 1}.",
-    status: 0
-  )
+  user.save! if user.new_record?
+
+  11.times do |j|
+    user.questions.find_or_create_by(
+      uuid: SecureRandom.uuid,
+      title: "This title is test_#{j + 1}.",
+      body: "This body is test_#{j + 1}.",
+      status: Question.statuses.keys.sample
+    )
+  end
 end

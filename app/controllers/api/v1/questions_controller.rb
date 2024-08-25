a273f62ef91d6eae0a6ca0faa9a81d89a8b15a6a
topi0247 <karubona.ra247@gmail.com>
2024-08-25 16:20:23 +0900

    fix:rubocopで修正

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,30 +1,36 @@
+# frozen_string_literal: true
+
 include Pagy::Backend
 
-class Api::V1::QuestionsController < Api::V1::BasesController
-  def index
-    current_page = params[:page] || 1
-    pagy, questions = pagy(Question.includes(:user).all, page: current_page)
-    render json: questions, each_serializer: QuestionSerializer
-  end
+module Api
+  module V1
+    class QuestionsController < Api::V1::BasesController
+      def index
+        current_page = params[:page] || 1
+        _, questions = pagy(Question.includes(:user).all, page: current_page)
+        render json: questions, each_serializer: QuestionSerializer
+      end
 
-  def create
-    question = @current_user.questions.new(question_params)
+      def create
+        question = @current_user.questions.new(question_params)
 
-    if question.save
-      render json: { uuid: question.uuid }, status: :created
-    else
-      render json: { message: question.errors.full_messages }, status: :unprocessable_entity
+        if question.save
+          render json: { uuid: question.uuid }, status: :created
+        else
+          render json: { message: question.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def count_all_questions
+        all_count = Question.count
+        render json: { count: all_count }
+      end
+
+      private
+
+      def question_params
+        params.require(:question).permit(:uuid, :title, :body, :status, :page)
+      end
     end
-  end
-
-  def count_all_questions
-    all_count = Question.count
-    render json: { count: all_count }
-  end
-
-  private
-
-  def question_params
-    params.require(:question).permit(:uuid, :title, :body, :status, :page)
   end
 end

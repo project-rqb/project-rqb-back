ddde44910b1a85419e0de9cc1de62b931f61bcff
三浦大河 <taigav8@gmail.com>
2024-09-03 10:29:31 +0900

    モデル作成およびuserモデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_many :questions
   has_many :answers
 
+  has_many :user_learning_tags
+  has_many :learning_tags, through: :user_learning_tags, source: :tag
+
+  has_many :user_learned_tags
+  has_many :learned_tags, through: :user_learned_tags, source: :tag
+
   validates :uid, presence: true, uniqueness: true
   validates :provider, presence: true
   validates :github_uid, presence: true, uniqueness: true

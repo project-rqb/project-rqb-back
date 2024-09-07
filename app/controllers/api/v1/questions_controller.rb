8d6d741837e3b6f5711a6c613f6d1868677b84e0
topi0247 <karubona.ra247@gmail.com>
2024-09-07 16:57:35 +0900

    feat:質問のレビューを行うPOSTメソッド追加

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -8,18 +8,17 @@ module Api
       def index
         current_page = params[:page] || 1
         order_by = params[:order] || 'new'
-        order_by = if order_by == 'new'
-                     'desc'
-                   else
-                     'asc'
-                   end
+        order_by = order_by == 'new' ? 'desc' : 'asc'
         _, questions = pagy(Question.includes(:user).all.order("created_at #{order_by}"), items: 10,
                                                                                           page: current_page)
         render json: questions, each_serializer: QuestionSerializer
       end
 
       def create
-        question = @current_user.questions.new(question_params)
+        question_params_without_tags = question_params.except(:tags)
+        question = @current_user.questions.new(question_params_without_tags)
+
+        # TODO: tagsを保存する処理を追加する
 
         if question.save
           render json: { uuid: question.uuid }, status: :created
@@ -33,10 +32,22 @@ module Api
         render json: { count: all_count }
       end
 
+      def ai_review
+        title = question_params[:title]
+        body = question_params[:body]
+        tags = question_params[:tags]
+        begin
+          review = Chatgpt.call(title, tags, body)
+          render json: { review: }, status: :ok
+        rescue Net::ReadTimeout
+          render json: { review: 'AIのレビューがタイムアウトしました。' }, status: :gateway_timeout
+        end
+      end
+
       private
 
       def question_params
-        params.require(:question).permit(:uuid, :title, :body, :status)
+        params.require(:question).permit(:uuid, :title, :body, :status, tags: [])
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
       get 'auth/:provider/callback', to: 'sessions#create'
       get 'auth/me', to: 'sessions#me'
 
-      resources :questions, only: [:index, :create] do
+      resources :questions, only: %i[index create] do
         resources :answers, only: [:create]
       end
       get 'questions/all_count', to: 'questions#count_all_questions'
+      post 'questions/ai_review', to: 'questions#ai_review'
     end
   end
 end
